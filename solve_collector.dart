0e30fc0ea2d46c249e8889ed778088f3d17fa896
Dominik Vidakovic <112127581+dvidakovic7@users.noreply.github.com>
2022-08-26 10:10:49 +0200

    Add files via upload

---
+++ solve_collector.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+double solveCollector (int solveNumber) {
+    print("\nEnter solve ${solveNumber}:\n");
+    double solve = double.parse(stdin.readLineSync()!);
+    return solve;
+} 
